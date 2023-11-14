13b81ed5db3b7115e25987bf787f7ec2eb4ae708
adel_5210 <adel_5210m@yahoo.com>
2023-11-14 13:33:46 +0800

    Create client q

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"asyncq_test/tasks"
+	"log"
+
+	"github.com/hibiken/asynq"
+)
+
+const redisAddr = "127.0.0.1:6379"
 
 func main() {
-	fmt.Println("test")
+	client := asynq.NewClient(asynq.RedisClientOpt{
+		Addr: redisAddr,
+	})
+	defer client.Close()
+
+	task, err := tasks.NewEmailDeliveryTask(42, "some:template:id")
+	if err != nil {
+		log.Fatalf("Could not create task : %v", err)
+	}
+
+	info, err := client.Enqueue(task)
+	if err != nil {
+		log.Fatalf("Could not enqueue task : %v", err)
+	}
+	log.Printf("Enqueued task: id: %s queue %s", info.ID, info.Queue)
 }
